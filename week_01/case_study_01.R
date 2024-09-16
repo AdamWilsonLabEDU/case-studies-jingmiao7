cbb7098b2c99e96347fb1f78e21b5e6c0f864a98
Jing Miao <jingmiao@buffalo.edu>
2024-09-16 17:02:16 -0400

    the result of first case study

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,3 +1,84 @@
-data(iris)
+data(iris) # load data
 
-##test
+#plot based on hist (default function)
+
+petal_length_mean <- mean (iris$Petal.Length) # calculate the mean value of Petal.Length
+hist(iris$Petal.Length) #create a histogram
+hist(iris$Petal.Length, breaks = 50, ylim = c(0, 20)) # set 50 breaks for the histogram and set the range of y
+abline(v = mean(iris$Petal.Length), col='red', lwd = 3) # add the average on the graph
+lines(density(iris$Petal.Length), col = 'green', lwd = 3) # add the density on the graph
+hist(iris$Petal.Length, breaks = 50, xlab = 'Length of Petal (cm)', ylab = 'Frequency', main = 'Distribution of Petal Length', col = 'gray', border = "white") #add the legends and title on the graph
+
+#install packages fo "ggplot2", "readr", and "dplyr", only need to do once
+install.packages("ggplot2")
+install.packages("readr")
+install.packages("dplyr")
+# load those packages, load by "library" before we use it in one project
+library(ggplot2)
+library(readr)
+library(dplyr)
+
+# plot the histogram based on ggplot
+# basic graph
+ggplot(iris, aes(x = Petal.Length)) + geom_histogram()
+length_stats <- iris |>
+  summarise(mean_length = mean(Petal.Length))
+length_stats
+
+# add average line by using geom_vline()
+ggplot(data = iris, aes(x = Petal.Length, y = after_stat(density))) +
+  geom_histogram() +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1)
+
+# add the density by using geom_density()
+ggplot(data = iris, aes(x = Petal.Length, y = after_stat(density))) +
+  geom_histogram() +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "green", linewidth = 1)
+
+# set 50 breaks for the x extent
+ggplot(data = iris, aes(x = Petal.Length, y = after_stat(density))) +
+  geom_histogram(bins = 50) +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "green", linewidth = 1)
+
+# change number of bins by changing the width of it
+ggplot(data = iris, aes(x = Petal.Length, y = after_stat(density))) +
+  geom_histogram(binwidth = 0.1) +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "green", linewidth = 1)
+
+ggplot(data = iris, aes(x = Petal.Length, y = after_stat(density))) +
+  geom_histogram(boundary = 0.5) +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "green", linewidth = 1)
+
+ggplot(data = iris, aes(x = Petal.Length, y = after_stat(density))) +
+  geom_histogram(bins = 50, color = "white", fill = "blue") +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "green", linewidth = 1)
+
+data_iris <- iris |>
+  mutate(Species = factor(Species))
+
+ggplot(data = data_iris, aes(x = Petal.Length, y = after_stat(density), fill = Species)) +
+  geom_histogram(bins = 50, color = "white", fill = "blue") +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "gray", linewidth = 1)+
+  labs(x ='Length of Petal (cm)', y='Count', title = 'Petal Length Distributions')
+
+ggplot(data = data_iris, aes(x = Petal.Length, y = after_stat(density), fill = Species)) +
+  geom_histogram(bins = 50, color = "white", fill = "blue") +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "gray", linewidth = 1)+
+  labs(x ='Length of Petal (cm)', y='Count', title = 'Petal Length Distributions') +
+  xlim(0, 8)
+
+ggplot(data = data_iris, aes(x = Petal.Length, y = after_stat(density), fill = Species)) +
+  geom_histogram(bins = 50, color = "white", fill = "blue") + facet_grid(vars(Species)) +
+  geom_vline(aes(xintercept = mean_length), length_stats, color = "red", linewidth = 1) +
+  geom_density(color = "gray", linewidth = 1)+
+  labs(x ='Length of Petal (cm)', y='Count', title = 'Petal Length Distributions') +
+  xlim(0, 8) +
+  theme(legend.position = "bottom")
+
