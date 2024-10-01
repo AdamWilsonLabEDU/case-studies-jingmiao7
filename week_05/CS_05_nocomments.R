ca9d9c19591e6d247a489e24e4833d082196c47e
Jing Miao <jingmiao@buffalo.edu>
2024-10-01 14:21:10 -0400

    First draft of case study5

---
+++ week_05/CS_05_nocomments.R
@@ -0,0 +1,54 @@
+# install.packages("spData")
+# install.packages("sf")
+
+library(spData)
+library(sf)
+library(tidyverse) # library(units) #this one is optional, but can help with unit conversions.
+
+#load 'world' data from spData package
+data(world)
+glimpse(world)
+# load 'states' boundaries from spData package
+data(us_states)
+glimpse(us_states)
+plot(world[1])  #plot if desired
+plot(us_states[1]) #plot if desired
+
+albers="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+canada <- world[world$name_long == "Canada", ]
+canada_albers <- st_transform(canada, crs = albers)
+canada_buffered <- st_buffer(canada_albers, dist = 10000)
+
+# Plot Canada and its buffer
+ggplot() +
+  geom_sf(data = canada, fill = "lightblue", color = "gray") +  # Original Canada
+  geom_sf(data = canada_buffered, fill = NA, color = "black", linetype = "dashed") +  # Buffered Canada
+  theme_minimal() +
+  ggtitle("Canada with 10 km Buffer")
+
+newyork <- us_states[us_states$NAME == "New York", ]
+newyork_proj <- st_transform(newyork, crs = albers)
+
+border <- st_intersection(canada_buffered, newyork_proj)
+
+ggplot() +
+  geom_sf(data = newyork_proj, fill = NA, color = "black") +
+  geom_sf(data = border, fill = "red", color = "black", alpha = 0.5) +
+  theme_minimal() +
+  ggtitle("Border Area Between Buffered Canada and New York")
+
+border_area <- st_area(border)
+border_area
+
+#install.packages("leaflet")
+library(leaflet)
+
+border_wgs84 <- st_transform(border, crs = 4326)
+
+leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap tiles
+  addPolygons(data = border_wgs84, color = "black", weight = 2, opacity = 0.7,
+              fillColor = "red", fillOpacity = 0.5, popup = ~as.character(st_area(border_wgs84))) %>%
+  addLegend(position = "bottomright", colors = "black", labels = "Border Area", title = "Legend") %>%
+  setView(lng = -75, lat = 45, zoom = 5)  # Set the center of the map to North America
